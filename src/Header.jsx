59ac506ba15e299cf0436f1e96a00c8858e83ba6
Óscar O <oscarforjuela@hotmail.com>
2022-12-19 21:41:06 -0500

    Dark mode

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,6 +5,18 @@ import { useTheme } from './Context/ThemeProvider';
 export default function Header({currencys, fun, cur}){
   const {theme, toggleTheme} = useTheme();
   
+  let bo = document.getElementsByTagName('body')[0];
+
+  function fondo(a){
+    if(a == 'light'){
+      bo.style.backgroundColor = '#f00';     
+    }else{
+      bo.style.backgroundColor = '#000';
+      bo.classList.add('modo-dark')
+    }
+    
+  }
+
   return (
     <header className='app-header'>
       <p>Crypto Stadistics</p>
@@ -12,7 +24,7 @@ export default function Header({currencys, fun, cur}){
       <select value={cur} name="coinSelect" id="coinSelect" onChange={_ => {fun(document.getElementById("coinSelect").value)}}>
         {currencys.map((item, index) => <option value={item} key={index} >{item}</option>)}  
       </select>
-      <button className='toogleMode' onClick={toggleTheme}>
+      <button className='toogleMode' onClick={fondo(toggleTheme)}>
         {theme.img}
       </button>
       </div>
